22f27ae502c99904ebfd952302cc9524080f30aa
XavZol <alansolis707@gmail.com>
2025-11-09 11:08:56 -0600

    While con python

--- Objet.py
+++ Objet.py
@@ -1,22 +1,23 @@
-nombre = input('Cual es tu nombre: \r\n')
+#While for 
 
-print(f'Hola {nombre}')
+pregunta = 'Agrega un numero y te dire si es par o impar\r\n'
+pregunta += ' (Escribe cerrar para salir de la aplicacion ) \r\n'
+preguntar = True
 
-#leer datos que seran numeros
-edad = input('Cual es tu edad\r\n')
-
-#Convertir un string a un entero
-edad = int(edad)
-
-if edad >= 18:
-    print('Eres mayor de edad')
-else:
-    print('Eres menor de edad')
+while preguntar:
     
-#Mezclar con operadores
-numero = input('Agrega un numero y te dire si es par o impar\r\n')
-numero = int(numero)
-if numero % 2 == 0:
-    print(f'El numero {numero} es par')
-else: 
-    print(f'El numero {numero} es impar')
+    numero = input(pregunta)
+
+    if numero == 'cerrar':
+        preguntar = False
+    else:
+        # 1. Convierte a entero SOLO si no es "cerrar"
+        numero = int(numero) 
+
+        # 2. Revisa si es par/impar DENTRO del else
+        if numero % 2 == 0:
+            print(f'El numero {numero} es par')
+        else: 
+            print(f'El numero {numero} es impar')
+
+print("¡Hasta luego!") # Mensaje de salida opcional
