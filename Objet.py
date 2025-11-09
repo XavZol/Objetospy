f22c830e0c84d4a47d26a6a38ff0b13138f608fb
XavZol <alansolis707@gmail.com>
2025-11-09 11:14:13 -0600

    while if con python

--- Objet.py
+++ Objet.py
@@ -1,23 +1,19 @@
 #While for 
 
-pregunta = 'Agrega un numero y te dire si es par o impar\r\n'
-pregunta += ' (Escribe cerrar para salir de la aplicacion ) \r\n'
-preguntar = True
+numero = 0
 
-while preguntar:
-    
-    numero = input(pregunta)
+#while numero <= 10:
+#    print(numero)
+#    numero += 1 
 
-    if numero == 'cerrar':
-        preguntar = False
+while numero <= 10:
+    if numero == 5:
+        print('CINCOOOO:')
     else:
-        # 1. Convierte a entero SOLO si no es "cerrar"
-        numero = int(numero) 
+        print(numero)
+    print(numero)
+    numero += 1 
+    
+    
 
-        # 2. Revisa si es par/impar DENTRO del else
-        if numero % 2 == 0:
-            print(f'El numero {numero} es par')
-        else: 
-            print(f'El numero {numero} es impar')
-
-print("¡Hasta luego!") # Mensaje de salida opcional
+    
