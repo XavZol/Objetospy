bad85d402f689b8571957e24fa34c7dc60e1bdc3
XavZol <alansolis707@gmail.com>
2025-11-10 22:09:28 -0600

    Getters y Setters con Private para hacer cambios en la clase

--- Objet.py
+++ Objet.py
@@ -3,20 +3,25 @@ class Restaurant:
     def __init__(self, nombre, categoria, precio):
         self.nombre = nombre # Atributo
         self.categoria = categoria
-        self._precio = precio # Default: Public, PROTECTED 
+        self.__precio = precio # Default: Public, PROTECTED, PRIVATE
 
     def mostrar_informacion(self):
-        print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self._precio}')
+        print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self.__precio}')
+
+    # GETTERS Y SETTERS - Get = Obtiene un valor, SET = Agrega un valor
+    def get_precio(self):
+        return self.__precio
+
+    def set_precio(self, precio):
+        self.__precio = precio
 
 #Instanciar la clase
 restaurant = Restaurant('Pizzeria Mexico', 'Comida Italiana', 50)
-
-print(restaurant._precio)
-restaurant._precio = 80
+# restaurant.__precio = 80 #No será posible modificarlo con PRIVATE
 restaurant.mostrar_informacion()
-
+restaurant.set_precio(80)
+precio = restaurant.get_precio()
+print(precio)
 
 restaurant2 = Restaurant('Hamburguesas Python', 'Comida Casual', 20)
-print(restaurant2._precio)
-restaurant2._precio = 40
 restaurant2.mostrar_informacion()
