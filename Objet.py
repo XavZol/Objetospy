5df98b8ad20bf48a290b2b060ae2b7d8eb22b36b
XavZol <alansolis707@gmail.com>
2025-11-10 22:08:31 -0600

    Public  con métodos, para hacer cambios en la clase

--- Objet.py
+++ Objet.py
@@ -3,14 +3,20 @@ class Restaurant:
     def __init__(self, nombre, categoria, precio):
         self.nombre = nombre # Atributo
         self.categoria = categoria
-        self.precio = precio
+        self._precio = precio # Default: Public, PROTECTED 
 
     def mostrar_informacion(self):
-        print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self.precio}')
+        print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self._precio}')
 
 #Instanciar la clase
 restaurant = Restaurant('Pizzeria Mexico', 'Comida Italiana', 50)
+
+print(restaurant._precio)
+restaurant._precio = 80
 restaurant.mostrar_informacion()
 
+
 restaurant2 = Restaurant('Hamburguesas Python', 'Comida Casual', 20)
+print(restaurant2._precio)
+restaurant2._precio = 40
 restaurant2.mostrar_informacion()
