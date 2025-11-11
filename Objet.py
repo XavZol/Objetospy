7a3522bd38f916a1bb39a1f4c599b7601226d0d8
XavZol <alansolis707@gmail.com>
2025-11-10 22:10:48 -0600

    Herencia en Python

--- Objet.py
+++ Objet.py
@@ -8,7 +8,30 @@ class Restaurant:
     def mostrar_informacion(self):
         print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self.__precio}')
 
+    # GETTERS Y SETTERSclass Restaurant:
+
+    def __init__(self, nombre, categoria, precio):
+        self.nombre = nombre # Atributo
+        self.categoria = categoria
+        self.__precio = precio # Default: Public, PROTECTED, PRIVATE
+
+    def mostrar_informacion(self):
+        print(f'Nombre: {self.nombre}, Categoria: {self.categoria}, Precio: {self.__precio}')
+
     # GETTERS Y SETTERS - Get = Obtiene un valor, SET = Agrega un valor
+    def get_precio(self):
+        return self.__precio
+
+    def set_precio(self, precio):
+        self.__precio = precio
+
+# Crear una clase hijo de Restaurant
+class Hotel(Restaurant):
+    def __init__(self, nombre, categoria, precio):
+        super().__init__(nombre, categoria, precio)
+
+hotel = Hotel('Hotel POO', '5 Estrellas', 200)
+hotel.mostrar_informacion() - Get = Obtiene un valor, SET = Agrega un valor
     def get_precio(self):
         return self.__precio
 
